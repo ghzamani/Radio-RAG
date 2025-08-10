5276896a08e8440ad8a16fb39e2de88d6e5b3349
Ghazal Zamaninejad <ghzamaninejad@gmail.com>
2025-08-10 16:05:11 +0330

    Edit eval to accept path as input, also add strip

--- eval.py
+++ eval.py
@@ -2,11 +2,9 @@ from evaluation import compute_scores
 from utills import read_jsonl_as_dict, extract_sections
 
 def lower_case(my_list):
-    return [item.lower() for item in my_list]
+    return [item.lower().strip() for item in my_list]
 
-def main():
-    # dir_name = "exp2_no_rag/ "
-    dir_name = "exp3/ "
+def evaluate_reports(dir_name: str):
     gold_impressions = list(read_jsonl_as_dict(dir_name + 'gold_impressions.jsonl').values())
     gold_findings = list(read_jsonl_as_dict(dir_name + 'gold_findings.jsonl').values())
     predicted_reports = read_jsonl_as_dict(dir_name + 'predicted_reports.jsonl')
@@ -24,13 +22,17 @@ def main():
     predicted_impressions = lower_case(predicted_impressions)
     predicted_findings = lower_case(predicted_findings)
 
+    print("Computing impression scores")
     impression_scores = compute_scores(gold_impressions, predicted_impressions,
                                        output_name=dir_name + "impression_scores.json")
+    print("Computing findings scores")
     findings_scores = compute_scores(gold_findings, predicted_findings,
                                      output_name=dir_name + "findings_scores.json")
     print("impression:", impression_scores)
     print("findings:", findings_scores)
 
+    return impression_scores, findings_scores
+
 
 if __name__ == '__main__':
-    main()
+    evaluate_reports("study_exp6/")
